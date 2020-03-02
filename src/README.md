14d97c6eea6b608aa2c0fc9748d390683da031fb
Ever Felipe Benites <everfelipe@gmail.com>
2020-03-02 05:53:06 -0400

    Arrumei o docker-composer agora o ambiente de dev esta OK

---
+++ src/README.md
@@ -0,0 +1,3 @@
+"# projeto01"  
+Neste repositori estão os arquivos e configurações do ambiente para o primeiro projeto pratico do curso full static da Danki Code
+
---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projeto Prático 01</title>
+</head>
+<body>
+    <?php
+       
+        echo "ever Felipe benites "
+    ?>
+</body>
+</html>
